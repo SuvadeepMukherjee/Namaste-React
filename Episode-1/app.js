ee94fef673576188d861ab2b21079491bd323853
Suvadeep <suvadeepm0709@gmail.com>
2025-02-08 20:14:17 +0530

    episode 1 complete

--- Episode-1/app.js
+++ Episode-1/app.js
@@ -7,14 +7,25 @@
 </div>; */
 }
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ])
-);
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "Hello"),
+//     React.createElement("h2", {}, "I am an h2 tag"),
+//   ])
+// );
+
+const parent = React.createElement("div", "parent", [
+  React.createElement("div", "child1", [
+    React.createElement("h1", {}, "heading1"),
+    React.createElement("h2", {}, "heading2"),
+  ]),
+  React.createElement("div", "child2", [
+    React.createElement("h1", {}, "heading1"),
+    React.createElement("h2", {}, "heading2"),
+  ]),
+]);
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading", xyz: "abc" },
